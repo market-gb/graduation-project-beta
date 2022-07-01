f3349cbdbd4acd1aeffb587a6c58f605a8e99f58
Maksim Shelepov <34706086+Loredan15@users.noreply.github.com>
2022-07-02 01:24:21 +0300

    Загружен шаблон проекта из альфа версии, добавлены модули Core и Gateway

---
+++ core-service/src/main/java/ru/market_gb/gateway/CoreSpringWebApplication.java
@@ -0,0 +1,11 @@
+package ru.market_gb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSpringWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSpringWebApplication.class, args);
+	}
+}
