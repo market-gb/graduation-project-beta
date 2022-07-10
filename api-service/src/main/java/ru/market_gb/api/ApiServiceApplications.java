7e507229b10eb13210b890287b67fc057a724dab
Andrey <aedbaykal@gmail.com>
2022-07-10 12:11:14 +0300

    GB-37-сделать-api-запускаемым-сервисом (иначе другие сервисы не смогут компилироваться)

---
+++ api-service/src/main/java/ru/market_gb/api/ApiServiceApplications.java
@@ -0,0 +1,11 @@
+package ru.market_gb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApplications {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApplications.class, args);
+    }
+}
